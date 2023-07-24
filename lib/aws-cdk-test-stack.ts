3655b3a80bf42e0bef7b257972b37337db8ceee3
kohei <kouhei.ikeda@veriserve.co.jp>
2023-07-24 15:20:52 +0900

    AWS CDK + Lambda + API Gateway で REST APIを作成

--- lib/aws-cdk-test-stack.ts
+++ lib/aws-cdk-test-stack.ts
@@ -1,16 +1,32 @@
 import * as cdk from 'aws-cdk-lib';
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsCdkTestStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Lambda関数
+    const nameHelloWorldFunc = "helloWorldFunc"
+    const func = new NodejsFunction(this, nameHelloWorldFunc, {
+      entry: "lib/hello-world.ts",
+      runtime: Runtime.NODEJS_18_X,
+      functionName: nameHelloWorldFunc,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkTestQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // API Gateway (REST API)
+    const restApi = new cdk.aws_apigateway.RestApi(this, "nameRestApi", {
+      deployOptions: {
+        stageName: 'v1',
+      },
+      restApiName: `Rest_API_with_Lambda`,
+    });
+
+    // API Gatewayにリクエスト先のリソースを追加
+    const restApiHelloWorld = restApi.root.addResource('hello_world');
+
+    // API GatewayのリソースにLambda関数を紐付け
+    restApiHelloWorld.addMethod("GET", new cdk.aws_apigateway.LambdaIntegration(func));
   }
 }
